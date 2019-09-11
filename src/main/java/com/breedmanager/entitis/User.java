87b49d0ceffb0e3272349e414e93ea4efb796509
milbork <milbork@gmail.com>
2019-09-11 16:11:31 +0200

    initial commit

---
+++ src/main/java/com/breedmanager/entitis/User.java
@@ -0,0 +1,4 @@
+package com.breedmanager.entitis;
+
+public class User {
+}
---
+++ src/main/java/com/breedmanager/interfaces/RegistrationInterface.java
@@ -0,0 +1,11 @@
+package com.breedmanager.interfaces;
+
+
+import com.breedmanager.entitis.Breeder;
+import com.breedmanager.entitis.User;
+
+public interface RegistrationInterface {
+
+    Breeder addBreeder(Breeder breeder);
+    User addUser(User user);
+}
